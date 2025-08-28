c7accd7d9d3013f8533df57c12bf9f199cf49041
FariqJavier <fariqjavier@gmail.com>
2025-08-28 17:20:51 +0700

    Handle option req to enable cors on dev and prod

--- api/chatbot.ts
+++ api/chatbot.ts
@@ -12,9 +12,12 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
